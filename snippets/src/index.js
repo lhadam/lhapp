9eae7dc5c8c032d61aacd894cb995359a188996f
lhadam <adam@logicherder.com>
2018-02-22 11:39:04 -0500

    Added test

--- snippets/src/index.js
+++ snippets/src/index.js
@@ -1,36 +1,6 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import './index.css';
-import { Popover, Button } from 'antd';
-
-class App extends React.Component {
-  state = {
-    visible: false,
-  }
-  hide = () => {
-    this.setState({
-      visible: false,
-    });
-  }
-  handleVisibleChange = (visible) => {
-    this.setState({ visible });
-  }
-  render() {
-    return (
-      <Popover
-        content={<a onClick={this.hide}>Close</a>}
-        title="Title New"
-        trigger="click"
-        visible={this.state.visible}
-        onVisibleChange={this.handleVisibleChange}
-      >
-        <Button type="primary">Click me</Button>
-      </Popover>
-    );
-  }
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('container'));
           
